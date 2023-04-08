886ca28baf4954cfc4adfe8ffe92b1335f35c1fe
Trần Quý <quytran52003@gmail.com>
2023-04-09 02:22:09 +0700

    Phần 22

--- laravelpro.sql
+++ laravelpro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th4 02, 2023 lúc 06:12 PM
+-- Thời gian đã tạo: Th4 08, 2023 lúc 09:21 PM
 -- Phiên bản máy phục vụ: 10.4.24-MariaDB
 -- Phiên bản PHP: 7.4.29
 
@@ -123,7 +123,7 @@ CREATE TABLE `tbl_posts` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `desc` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `creator` bigint(20) UNSIGNED NOT NULL,
-  `post_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -131,12 +131,11 @@ CREATE TABLE `tbl_posts` (
 -- Đang đổ dữ liệu cho bảng `tbl_posts`
 --
 
-INSERT INTO `tbl_posts` (`id`, `created_at`, `updated_at`, `desc`, `creator`, `post_name`, `deleted_at`) VALUES
-(1, NULL, NULL, NULL, 1, 'Tại sao bạn cần phải học', NULL),
-(2, NULL, NULL, NULL, 2, 'Tiếng anh quan trọng như nào?', NULL),
-(3, NULL, NULL, NULL, 3, 'Sách 10 vạn câu hỏi vì sao', NULL),
-(4, NULL, NULL, NULL, 4, 'Sách giáo dục trẻ nhỏ', NULL),
-(5, '2023-03-31 11:54:19', '2023-03-31 12:03:04', NULL, 1, 'Trần Quang Quý', NULL);
+INSERT INTO `tbl_posts` (`id`, `created_at`, `updated_at`, `desc`, `creator`, `title`, `deleted_at`) VALUES
+(7, '2023-04-07 17:39:55', '2023-04-07 17:39:55', '<p>&nbsp;</p>\r\n<p style=\"text-align: center;\"><span style=\"color: #2dc26b;\"><strong><span style=\"text-decoration: underline;\">&Ocirc;ng Mykhailo Podolyak, một quan chức của văn ph&ograve;ng tổng thống Ukraine, ng&agrave;y 7/4 nghi ngờ th&ocirc;ng tin trong c&aacute;i gọi l&agrave; bản kế hoạch r&ograve; rỉ \"giống chiến dịch tung th&ocirc;ng tin sai lệch của Nga nhằm gieo rắc nghi ngờ về kế hoạch phản c&ocirc;ng của Ukraine\".</span></strong></span></p>\r\n<p style=\"text-align: center;\"><span style=\"color: #2dc26b;\"><strong><span style=\"text-decoration: underline;\"><img src=\"/laravel-unitop.vn/public/storage/photos/avtfb.jpg\" alt=\"\" width=\"239\" height=\"240\" /></span></strong></span></p>', 1, 'Ukraine lên tiếng sau nghi vấn tài liệu mật của Mỹ - NATO bị rò rỉ', NULL),
+(8, '2023-04-07 17:42:13', '2023-04-07 17:42:13', '<p>&nbsp;</p>\r\n<p style=\"text-align: center;\"><iframe src=\"https://www.youtube.com/embed/q_0xeZNj65s\" width=\"560\" height=\"314\" allowfullscreen=\"allowfullscreen\"></iframe></p>\r\n<p style=\"text-align: center;\">&nbsp;</p>', 1, 'Nhạc trẻ tổng hợp hay nhất 2023', NULL),
+(9, '2023-04-08 14:54:14', '2023-04-08 14:54:14', '<p>sdfsd</p>', 1, 'sdfsd', NULL),
+(10, '2023-04-08 14:59:39', '2023-04-08 14:59:39', '<p>sdfsd</p>', 1, 'sdfsd', NULL);
 
 -- --------------------------------------------------------
 
@@ -161,24 +160,16 @@ CREATE TABLE `tbl_products` (
 --
 
 INSERT INTO `tbl_products` (`id`, `name_product`, `content`, `price`, `product_cat_parent`, `creator`, `created_at`, `updated_at`, `thumb_main`) VALUES
-(2, 'IPhone 14 Pro Max 128G', NULL, NULL, 1, 1, '2023-04-02 11:39:43', '2023-04-02 11:39:43', 'public/uploads/pc3_thumb_main.png'),
-(3, 'IPhone 11 Pro', NULL, NULL, 1, 1, '2023-04-02 11:39:57', '2023-04-02 11:39:57', 'public/uploads/pc3_thumb_main.png'),
-(4, 'Macbook m1', NULL, NULL, 2, 1, '2023-04-02 11:40:13', '2023-04-02 11:40:13', 'public/uploads/pk3_thumb_detail4.png'),
-(5, 'Macbook m2', NULL, NULL, 2, 1, '2023-04-02 11:41:25', '2023-04-02 11:41:25', 'public/uploads/pk3_thumb_detail4.png'),
-(6, 'OPPO A92', NULL, NULL, 2, 1, '2023-04-02 12:03:26', '2023-04-02 12:03:26', 'public/uploads/pk2_thumb_detail1.png'),
-(7, 'Macbook m3', NULL, NULL, 2, 3, '2023-04-02 12:04:12', '2023-04-02 12:04:12', 'public/uploads/avtfb.jpg'),
-(8, 'Vsmart V1', NULL, NULL, 2, 3, '2023-04-02 13:51:58', '2023-04-02 13:51:58', 'public/uploads/pc9_thumb_detail1.png'),
-(9, 'Vinfast Lux A 2.0', NULL, NULL, 2, 3, '2023-04-02 13:56:42', '2023-04-02 13:56:42', 'public/uploads/pc1_thumb_detail1.png'),
-(10, 'Vinfast Lux A 2.0', NULL, NULL, 2, 3, '2023-04-02 14:15:09', '2023-04-02 14:15:09', 'public/uploads/pc1_thumb_detail1.png'),
-(11, 'Vinfast Lux A 2.0', NULL, NULL, 2, 3, '2023-04-02 14:17:36', '2023-04-02 14:17:36', 'public/uploads/pc1_thumb_detail1.png'),
-(12, 'Vinfast e34', NULL, NULL, 2, 3, '2023-04-02 14:41:48', '2023-04-02 14:41:48', 'public/uploads/pk4_thumb_detail3.png'),
-(13, 'Vinfast e34', NULL, NULL, 2, 3, '2023-04-02 14:42:03', '2023-04-02 14:42:03', 'public/uploads/pk4_thumb_detail3.png'),
-(14, 'Vinfast e34', NULL, NULL, 2, 3, '2023-04-02 15:12:38', '2023-04-02 15:12:38', 'public/uploads/pk4_thumb_detail3.png'),
-(15, 'Vinfast e34', NULL, NULL, 2, 3, '2023-04-02 15:14:02', '2023-04-02 15:14:02', 'public/uploads/pk4_thumb_detail3.png'),
-(16, 'Vinfast e34', NULL, NULL, 2, 3, '2023-04-02 15:14:16', '2023-04-02 15:14:16', 'public/uploads/pk5_thumb_detail3.png'),
-(17, 'Vinfast e34', NULL, NULL, 2, 3, '2023-04-02 15:14:38', '2023-04-02 15:14:38', 'public/uploads/pk5_thumb_detail3.png'),
-(18, 'Vinfast e34', NULL, NULL, 2, 3, '2023-04-02 15:15:02', '2023-04-02 15:15:02', 'public/uploads/pk5_thumb_detail3.png'),
-(19, 'Vinfast e34', NULL, NULL, 2, 3, '2023-04-02 15:15:16', '2023-04-02 15:15:16', 'public/uploads/pk4_thumb_main.png');
+(2, 'IPhone 14 Pro Max 128G', NULL, '2000000', 1, 1, '2023-04-02 11:39:43', '2023-04-02 11:39:43', 'public/uploads/pc3_thumb_main.png'),
+(3, 'IPhone 11 Pro', NULL, '3000000', 1, 1, '2023-04-02 11:39:57', '2023-04-02 11:39:57', 'public/uploads/pc3_thumb_main.png'),
+(4, 'Macbook m1', NULL, '18000000', 2, 1, '2023-04-02 11:40:13', '2023-04-02 11:40:13', 'public/uploads/pk3_thumb_detail4.png'),
+(5, 'Macbook m2', NULL, '23600000', 2, 1, '2023-04-02 11:41:25', '2023-04-02 11:41:25', 'public/uploads/pk3_thumb_detail4.png'),
+(6, 'OPPO A92', NULL, '7650000', 2, 1, '2023-04-02 12:03:26', '2023-04-02 12:03:26', 'public/uploads/pk2_thumb_detail1.png'),
+(7, 'Macbook m3', NULL, '16500000', 2, 3, '2023-04-02 12:04:12', '2023-04-02 12:04:12', 'public/uploads/avtfb.jpg'),
+(8, 'Vsmart V1', NULL, '2500000', 2, 3, '2023-04-02 13:51:58', '2023-04-02 13:51:58', 'public/uploads/pc9_thumb_detail1.png'),
+(9, 'Vinfast Lux A 2.0', NULL, '960000000', 2, 3, '2023-04-02 13:56:42', '2023-04-02 13:56:42', 'public/uploads/pc1_thumb_detail1.png'),
+(12, 'Vinfast e34', NULL, '860000000', 2, 3, '2023-04-02 14:41:48', '2023-04-02 14:41:48', 'public/uploads/pk4_thumb_detail3.png'),
+(20, 'Macbook m4', NULL, '42000000', 2, 3, '2023-04-04 10:53:35', '2023-04-04 10:53:35', 'public/uploads/pc1_thumb_detail1.png');
 
 -- --------------------------------------------------------
 
@@ -320,13 +311,13 @@ ALTER TABLE `tbl_cat_product`
 -- AUTO_INCREMENT cho bảng `tbl_posts`
 --
 ALTER TABLE `tbl_posts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `tbl_products`
 --
 ALTER TABLE `tbl_products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT cho bảng `tbl_roles`
